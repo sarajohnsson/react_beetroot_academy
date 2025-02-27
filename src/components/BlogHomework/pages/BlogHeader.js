c979fd424b04a4d1031f3804b33449a743d6cb3e
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-02-27 17:19:06 +0100

    Blog homework

--- src/components/BlogHomework/pages/BlogHeader.js
+++ src/components/BlogHomework/pages/BlogHeader.js
@@ -1,11 +1,18 @@
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import './BlogHeader.scss';
 
 export default function BlogHeader({ setPage }) {
     return (
         <nav>
-            <ul>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/singleblogpage">Single</NavLink>
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/">
+                        <FontAwesomeIcon icon={faHome} className="fa-icon" />
+                        Home
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
--- src/components/BlogHomework/pages/SingleBlog/SingleBlogPage.js
+++ src/components/BlogHomework/pages/SingleBlog/SingleBlogPage.js
@@ -1,13 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './SingleBlogPage.scss';
 
 export default function SingleBlogPage() {
     const params = useParams();
+    const [blogData, setBlogData] = useState({});
+
     function getBlogData() {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+            .then((res) => res.json())
+            .then((res) => setBlogData(res))
+            .catch((err) => console.error(err));
         console.log(params);
     }
 
     useEffect(getBlogData, []);
 
-    return <p>Single Blog page</p>;
+    return (
+        <div className="page-wrapper">
+            <div className="single-blog-card">
+                <h2 className="single-blog-title">{blogData.title}</h2>
+                <p className="single-blog-text">{blogData.body}</p>
+            </div>
+        </div>
+    );
 }
